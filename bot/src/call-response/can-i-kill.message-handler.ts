518646f257281cd4c059e4cdfa86c22c328d938d
Mitch Ferrer <mr.ferfer@gmail.com>
2018-03-10 17:59:31 -0700

    no ignores spaces

--- bot/src/call-response/can-i-kill.message-handler.ts
+++ bot/src/call-response/can-i-kill.message-handler.ts
@@ -15,19 +15,22 @@ export class CanIKillHandler extends BaseIncredMessageHandler {
       'kingdom',
       'churn',
       'teaparty',
-      'tea party',
+      'stripclub',
       'tp',
       'defiance',
       'tekniques'
     ];
 
     let normalized = msg.content.toLocaleLowerCase().trim();
-    var clanName = normalized.replace('can i kill', '').trim();
+    var clanName = normalized
+    .replace('can i kill', '')
+    .replace(/\s/g, '')
+    .trim();
 
     if (ally.filter(x => x == clanName).length > 0) {
       msg.channel.send(`Hey, ${msg.member} You cannot kill ${clanName}`);
     } else {
-      msg.channel.send(`Hey, ${msg.member} Party on! Kill Kill Kill`);
+      msg.channel.send(`Hey, ${msg.member} Party on! Kill Kill Kill ${clanName}`);
     }
   }
 }
