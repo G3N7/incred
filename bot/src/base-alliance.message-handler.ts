3f5c05042d0beebb6de09d992ce60a7ad0e6b0d2
Mitch Ferrer <mr.ferfer@gmail.com>
2018-04-08 19:28:31 -0600

    fixing roles

--- bot/src/base-alliance.message-handler.ts
+++ bot/src/base-alliance.message-handler.ts
@@ -2,14 +2,10 @@ import { BaseMessageHandler } from './base.message-handler';
 
 export abstract class BaseAllianceMessageHandler extends BaseMessageHandler {
   requiredRoles = [
-    'Incredibles-Leader',
-    'Incredibles-Squire',
-    'Incredibles-Captain',
-    'Incredibles-Knight',
-    'Kingdom-Leader',
-    'Kingdom-Squire',
-    'Kingdom-Captain',
-    'Kingdom-Knight'
+    'Leader',
+    'Squire',
+    'Captain',
+    'Knight'    
   ];
 
   allowedChannels: string[] = [];
