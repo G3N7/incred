e8dce51ae1d603eae913f6022f3c085b5eec208e
Mitch Ferrer <mr.ferfer@gmail.com>
2018-10-02 22:57:49 -0600

    removing old officer from the acceptable officer channel

--- bot/src/base-officer.message-handler.ts
+++ bot/src/base-officer.message-handler.ts
@@ -3,7 +3,6 @@ import { BaseMessageHandler } from './base.message-handler';
 export abstract class BaseOfficerMessageHandler extends BaseMessageHandler {
   allowedChannels = [
     'officers',
-    'oldofficer',
     'general'
   ];
 }
